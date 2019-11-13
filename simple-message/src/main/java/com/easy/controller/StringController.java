44902166b7b5a2166271b186ddd957f11c1bb3f0
aarashfarahani <arashfarahani@hotmail.com>
2019-11-13 12:14:07 +0330

    consumer that use listener has been added

--- simple-message/src/main/java/com/easy/controller/StringController.java
+++ simple-message/src/main/java/com/easy/controller/StringController.java
@@ -3,16 +3,15 @@ package com.easy.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/string")
 public class StringController {
-    @Autowired private KafkaTemplate<String, String> kafkaTemplate;
-    @Value("${topic.name}") private String topicName;
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    @Value("${topic.name}")
+    private String topicName;
 
     @PostMapping("/publish")
     public void publish(@RequestParam("message") String message) {
---
+++ simple-message/src/main/java/com/easy/service/Consumer.java
@@ -0,0 +1,13 @@
+package com.easy.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+    @KafkaListener(topics = "testTopic", groupId = "testTopicGroup")
+    public void consume(String message){
+        System.out.println("**********************************************************************************");
+        System.out.println(message);
+    }
+}
