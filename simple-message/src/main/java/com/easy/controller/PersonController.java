f36078b8a2468a9edf146dc853cf8bc1ff901716
aarashfarahani <arashfarahani@hotmail.com>
2019-11-13 11:55:34 +0330

    object to json then send to kafka has been completed

--- simple-message/src/main/java/com/easy/controller/PersonController.java
+++ simple-message/src/main/java/com/easy/controller/PersonController.java
@@ -1,21 +1,25 @@
 package com.easy.controller;
 
+import com.easy.model.Person;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/string")
-public class StringController {
+@RequestMapping("/person")
+public class PersonController {
+    @Autowired private Gson gson;
     @Autowired private KafkaTemplate<String, String> kafkaTemplate;
     @Value("${topic.name}") private String topicName;
 
     @PostMapping("/publish")
-    public void publish(@RequestParam("message") String message) {
-        this.kafkaTemplate.send(this.topicName, message);
+    public void publish(@RequestBody Person person) {
+        String json = this.gson.toJson(person);
+        this.kafkaTemplate.send(this.topicName, json);
     }
 }
